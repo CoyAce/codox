93633a9cdeafebce4d93e89a7619dfaac2dee32f
CoyAce <AkeyCoy@gmail.com>
2025-04-30 17:10:24 +0800

    update deploy script

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-scp -r public root@coyace.top:/var/www/html/doc
+ssh root@coyace.top << EOF
+cd repos
+git clone git@github.com:CoyAce/codox.git
+cd codox
+hugo
+mv -f public /var/www/html/doc
+EOF
